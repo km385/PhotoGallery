330fce0ba700a8d38d58d639f54090ae662cb6bb
Jakub Furtak <kuba.furtak.pl@gmail.com>
2022-08-14 14:14:09 +0200

    repo created

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.photogallery;
+
+import androidx.fragment.app.Fragment;
+
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
